1aa900a9630c9c2b81e93ed6f041c5b19001c6de
aleal <adriano.leal@exlcusivkey.pt>
2018-07-25 22:51:49 +0100

    styled-components

--- module_02/src/App.js
+++ module_02/src/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './styles/global';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>
-          Hello World!
-        </h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>
+      Hello World!
+    </h1>
+  </div>
+);
 
 export default App;
