344cd82027c47f5c87f812accbf8bf47fb802d55
aleal <adriano.leal@exlcusivkey.pt>
2018-07-30 14:48:53 +0100

    Utilizando bindActionCreators

--- module_03/src/TodoList.js
+++ module_03/src/TodoList.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
+
 const TodoList = ({ todos, addTodo }) => (
   <Fragment>
     <ul>
@@ -32,9 +35,7 @@ const mapStateToPro = state => ({
   todos: state.todos,
 });
 
-const mapDispachToProps = dispatch => ({
-  addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } }),
-});
+const mapDispachToProps = dispatch => bindActionCreators(TodoActions, dispatch);
 
 export default connect(
   mapStateToPro,
