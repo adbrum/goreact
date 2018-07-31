b1def0a332ea8295d884e60abec511dad01695d1
aleal <adriano.leal@exlcusivkey.pt>
2018-07-31 15:01:45 +0100

    Configuração do Redux

--- module_03/src/pages/main/index.js
+++ module_03/src/pages/main/index.js
@@ -1,11 +1,34 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
 
 class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ).isRequired,
+  };
+
   state = {
     repositoryInput: '',
   };
 
+  handleAddRepository = (e) => {
+    e.preventDefault();
+    this.props.addFavorite();
+  };
+
   render() {
+    const { favorites } = this.props;
     const { repositoryInput } = this.state;
     return (
       <Fragment>
@@ -20,22 +43,33 @@ Adicionar
           </button>
         </form>
         <ul>
-          <li>
-            <p>
-              <strong>
-facebook/react
-              </strong>
-              (A declarative, efficient, and flexible JavaScript library for building user
-              interfaces. )
-            </p>
-            <a href="http://github.com/facebook/react">
-Acessar
-            </a>
-          </li>
+          {favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>
+                  {favorite.name}
+                </strong>
+                (
+                {favorite.description}
+                )
+              </p>
+              {favorite.url}
+              Acessar
+            </li>
+          ))}
         </ul>
       </Fragment>
     );
   }
 }
 
-export default Main;
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
