253905340218775193da164d0e5ca5e2e28e5086
aleal <adriano.leal@exlcusivkey.pt>
2018-08-07 17:54:04 +0100

    Organizando estrutura

--- module_04/src/App.js
+++ module_04/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-
 import React from "react";
 
+import "./styles/global";
+
 const App = props => {
-  return <div />;
+  return <div>Hello World!</div>;
 };
 
 export default App;
