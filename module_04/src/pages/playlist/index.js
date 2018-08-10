0975e2ff7ac511d3ff94261ca6b9988e3f6cef44
aleal <adriano.leal@exlcusivkey.pt>
2018-08-10 11:49:19 +0100

    Configurando Redux & Saga

--- module_04/src/pages/playlist/index.js
+++ module_04/src/pages/playlist/index.js
@@ -9,33 +9,19 @@ const PlayList = () => (
     <Header>
       <img src="https://img.cdandlp.com/2015/05/imgL/117530031.jpg" alt="The Cure" />
       <div>
-        <span>
-PLAYLIST
-        </span>
-        <h1>
-Rock Forever
-        </h1>
-        <p>
-13 músicas
-        </p>
-        <button type="button">
-PLAY
-        </button>
+        <span>PLAYLIST</span>
+        <h1>Rock Forever</h1>
+        <p>13 músicas</p>
+        <button type="button">PLAY</button>
       </div>
     </Header>
     <SongList cellPadding={0} cellSpacing={0}>
       <thead>
         <th />
 
-        <th>
-Título
-        </th>
-        <th>
-Artista
-        </th>
-        <th>
-Álbum
-        </th>
+        <th>Título</th>
+        <th>Artista</th>
+        <th>Álbum</th>
         <th>
           <img src={ClockIcon} alt="Duração" />
         </th>
@@ -46,86 +32,46 @@ Artista
           <td>
             <img src={PlusIcon} alt="Adicionar" />
           </td>
-          <td>
-The Cure
-          </td>
-          <td>
-Boys Don't cry
-          </td>
-          <td>
-Boys Don't Cry (álbum)
-          </td>
-          <td>
-3:26
-          </td>
+          <td>The Cure</td>
+          <td>Boys Don't cry</td>
+          <td>Boys Don't Cry (álbum)</td>
+          <td>3:26</td>
         </tr>
         <tr>
           <td>
             <img src={PlusIcon} alt="Adicionar" />
           </td>
-          <td>
-The Cure
-          </td>
-          <td>
-Boys Don't cry
-          </td>
-          <td>
-Boys Don't Cry (álbum)
-          </td>
-          <td>
-3:26
-          </td>
+          <td>The Cure</td>
+          <td>Boys Don't cry</td>
+          <td>Boys Don't Cry (álbum)</td>
+          <td>3:26</td>
         </tr>
         <tr>
           <td>
             <img src={PlusIcon} alt="Adicionar" />
           </td>
-          <td>
-The Cure
-          </td>
-          <td>
-Boys Don't cry
-          </td>
-          <td>
-Boys Don't Cry (álbum)
-          </td>
-          <td>
-3:26
-          </td>
+          <td>The Cure</td>
+          <td>Boys Don't cry</td>
+          <td>Boys Don't Cry (álbum)</td>
+          <td>3:26</td>
         </tr>
         <tr>
           <td>
             <img src={PlusIcon} alt="Adicionar" />
           </td>
-          <td>
-The Cure
-          </td>
-          <td>
-Boys Don't cry
-          </td>
-          <td>
-Boys Don't Cry (álbum)
-          </td>
-          <td>
-3:26
-          </td>
+          <td>The Cure</td>
+          <td>Boys Don't cry</td>
+          <td>Boys Don't Cry (álbum)</td>
+          <td>3:26</td>
         </tr>
         <tr>
           <td>
             <img src={PlusIcon} alt="Adicionar" />
           </td>
-          <td>
-The Cure
-          </td>
-          <td>
-Boys Don't cry
-          </td>
-          <td>
-Boys Don't Cry (álbum)
-          </td>
-          <td>
-3:26
-          </td>
+          <td>The Cure</td>
+          <td>Boys Don't cry</td>
+          <td>Boys Don't Cry (álbum)</td>
+          <td>3:26</td>
         </tr>
       </tbody>
     </SongList>
